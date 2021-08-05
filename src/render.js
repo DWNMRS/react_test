03f7b475b30f2709ebaa396741824cab32ad1db8
DWNMRS <Downmars@yandex.ru>
2021-08-05 19:08:54 +0400

    added config and baseUrl

--- src/render.js
+++ src/render.js
@@ -6,8 +6,10 @@ import { addPost } from './redux/state';
 import { BrowserRouter } from "react-router-dom";
 
 export let renderEntireTree = (state) => {
+  const base = process.env.NODE_ENV === 'development' ? '' : 'react_test'
+  
   ReactDOM.render(
-    <BrowserRouter>
+    <BrowserRouter basename={base}>
       <App state={state} addPost={addPost} />
     </BrowserRouter>,
     document.getElementById('root'));
